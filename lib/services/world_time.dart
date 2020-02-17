0296bd6171dc977ff9156d66a75cf7ca56a40312
Md Zaid Ansari <zaidansari51097@gmail.com>
2020-02-17 23:25:00 +0530

    added try and catch block for error handling

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -13,15 +13,26 @@ class WorldTime{
 
 
 
-  Future<void> setTime() async {
-    Response response1 = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map timedata = jsonDecode(response1.body);
+  Future<void> setTime() async
+  {
+    try {
+      Response response1 = await get(
+          'http://worldtimeapi.org/api/timezone/$url');
+      Map timedata = jsonDecode(response1.body);
 
-    String datetime = timedata['datetime'];
-    String offset = timedata['utc_offset'];
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset.substring(1,3)) ,minutes: int.parse(offset.substring(4,6))));
-    time = now.toString();
+      String datetime = timedata['datetime'];
+      String offset = timedata['utc_offset'];
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset.substring(1, 3)),
+          minutes: int.parse(offset.substring(4, 6))));
+      time = now.toString();
+    }
+
+    catch(e){
+      print('Exception found: $e');
+      time='''can't fetch time from server''';
+    }
+
   }
 
 
